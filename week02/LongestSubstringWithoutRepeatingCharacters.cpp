055d0322e20e702d140876e95759c8cfa2d477ee
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2022-12-11 12:04:38 +0600

    Update LongestSubstringWithoutRepeatingCharacters.cpp

--- week02/LongestSubstringWithoutRepeatingCharacters.cpp
+++ week02/LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,13 +1,11 @@
 // O(N)
-// O(256)
+// O(1)
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
 
-        int frequency[256];
-        for(int i = 0; i < 256; i++)
-            frequency[i] = 0;
+        vector <int> frequency(256, 0); // For all ASCII characters
         
         int longestSubstringLength = 0;
         for(int L = 0, R = 0; R < s.size(); R++){
