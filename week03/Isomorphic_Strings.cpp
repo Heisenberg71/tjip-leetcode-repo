e2d1c92bcedcd7dbfd187f931ea012c72c27aa94
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2022-12-22 17:20:52 +0600

    Create Isomorphic_Strings.cpp

---
+++ week03/Isomorphic_Strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    static const int MAX_CHAR_LIM = 256;
+
+    bool isIsomorphic(string s, string t) {
+        vector <int> replaceMap_S(MAX_CHAR_LIM, 0), replaceMap_T(MAX_CHAR_LIM, 0);
+        for(int i = 0; i < s.size(); i++){
+            replaceMap_S[ s[i] ] = t[i];
+            replaceMap_T[ t[i] ] = s[i];
+        }
+        
+        for(int i = 0; i < s.size(); i++){
+            if(replaceMap_S[ s[i] ] != t[i] || replaceMap_T[ t[i] ] != s[i]) return false;
+        }
+        return true;
+    }
+};
