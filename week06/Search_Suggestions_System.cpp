10db8f89c3ffb6c3019cd038853aa5513bdf71ea
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-01-29 16:24:00 +0600

    Update Search_Suggestions_System.cpp

--- week06/Search_Suggestions_System.cpp
+++ week06/Search_Suggestions_System.cpp
@@ -1,3 +1,8 @@
+//O(P + S*MAX_P*3)  P = number of all characters in products vector of strings
+                 // S = size of searchString
+                 // MAX_P = max string length of products vector of strings
+
+
 class Solution {
 private:
     const static int LIM = 26;
@@ -23,16 +28,13 @@ private:
         currNode -> hasEnd = true;
     }
 
-    void getSuggestions(trieNode* currNode, vector <string> &suggestion, int& cnt, string& prefix){
-        if(cnt == 3 || currNode == nullptr) return;
-        if(currNode -> hasEnd){
-            suggestion.push_back(prefix);
-            cnt++;
-        }
+    void getSuggestions(trieNode* currNode, vector <string> &suggestion, string& prefix){
+        if(suggestion.size() == 3 || currNode == nullptr) return;
+        if(currNode -> hasEnd) suggestion.push_back(prefix);
         for(int ch = 0; ch < LIM; ch++){
             if(currNode -> next[ch] != nullptr){
                 prefix.push_back((char)(ch + 'a'));
-                getSuggestions(currNode -> next[ch], suggestion, cnt, prefix);
+                getSuggestions(currNode -> next[ch], suggestion, prefix);
                 prefix.pop_back();
             }
         }
@@ -51,8 +53,7 @@ public:
             prefix.push_back(searchWord[i]);
             if(currNode != nullptr) currNode = currNode -> next[ searchWord[i] - 'a' ];
             vector <string> suggestion;
-            int cnt = 0;
-            getSuggestions(currNode, suggestion, cnt, prefix);
+            getSuggestions(currNode, suggestion, prefix);
             allSuggestions.push_back(suggestion);
         }
 
