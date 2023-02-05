46edfc16c34602523e2e62ca58304852ed4335d5
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-02-05 14:56:29 +0600

    Update Number_of_Matching_Subsequences.cpp

--- week06/Number_of_Matching_Subsequences.cpp
+++ week06/Number_of_Matching_Subsequences.cpp
@@ -5,12 +5,11 @@
 class Solution {
 public:
     vector <int> index_of[26];
-    vector <int> :: iterator it;
 
     bool isMatched(string &currentWord){
         int currentIndex = -1;
         for(int i = 0; i < currentWord.size(); i++){
-            it = lower_bound(index_of[ currentWord[i] - 'a' ].begin(), index_of[ currentWord[i] - 'a' ].end(), currentIndex + 1);
+            auto it = lower_bound(index_of[ currentWord[i] - 'a' ].begin(), index_of[ currentWord[i] - 'a' ].end(), currentIndex + 1);
             if(it == index_of[ currentWord[i] - 'a' ].end()) return 0;
             currentIndex = *it;
         }
