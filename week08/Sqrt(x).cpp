eab836370254cfd4f325f84d245547612f48b461
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-02-21 20:31:10 +0600

    Update Sqrt(x).cpp

--- week08/Sqrt(x).cpp
+++ week08/Sqrt(x).cpp
@@ -1,9 +1,12 @@
+// T.C: O(log2(x)) Here, x = value of x
+// M.C: O(1)
+
 class Solution {
 public:
     int mySqrt(int x) {
         if(x == 0) return 0;
         
-        int lo = 1, hi = (int)((1LL << 31) - 1), squareRoot;
+        int lo = 1, hi = x, squareRoot;
         while(lo <= hi){
             int md = lo + (hi - lo)/2;
             if(md <= x/md){
