0ead5e149b1dd39bce7b9e49774754b97c1fbacf
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-02-24 01:54:41 +0600

    Create Employee_Importance.cpp

---
+++ week09/Employee_Importance.cpp
@@ -0,0 +1,25 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+private:
+    unordered_map <int, Employee*> id_employee;
+    int DFS(int u, vector<Employee*> employees){
+        int subtreeSum = id_employee[u] -> importance;
+        for(auto v: id_employee[u] -> subordinates)
+            subtreeSum += DFS(v, employees);
+        return subtreeSum;
+    }
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        for(auto emp: employees) id_employee[emp -> id] = emp;
+        return DFS(id, employees);
+    }
+};
