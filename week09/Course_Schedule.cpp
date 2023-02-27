b07d9735798d383d046553b9403958f70ba19d74
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-02-27 21:18:50 +0600

    Update Course_Schedule.cpp

--- week09/Course_Schedule.cpp
+++ week09/Course_Schedule.cpp
@@ -1,4 +1,4 @@
-// T.C: O(V + E) Here, V = number of nodes in the graph
+// T.C: O(V + E*log(V)) Here, V = number of nodes in the graph
 // M.C: O(V + E) Here, E = number of edge int the graph
 
 class Solution {
@@ -7,29 +7,28 @@ private:
     const static int PROCESSING = 1;
     const static int FINISHED_PROCESSE = 2;
     
-    bool dfs(int u, vector <int> adjMat[], vector <int> &vis)
+    bool dfs(int u, map<int, vector <int> > node_adjMat, vector <int> &vis)
     {
         if(vis[u] == PROCESSING) return true;
         if(vis[u] == FINISHED_PROCESSE) return false;
         vis[u] = PROCESSING;
         bool findCycle = false;
-        for(auto v: adjMat[u]){
-            findCycle |= dfs(v, adjMat, vis);
+        for(auto v: node_adjMat[u]){
+            findCycle |= dfs(v, node_adjMat, vis);
         }
         vis[u] = FINISHED_PROCESSE;
         return findCycle;
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector <int> adjMat[ numCourses ];
+        map<int, vector <int> > node_adjMat;
         vector <int> vis(numCourses, 0);
-        for(auto u: prerequisites){
-            adjMat[ u[0] ].push_back(u[1]);
-        }
+        for(auto u: prerequisites)
+            node_adjMat[ u[0] ].push_back(u[1]);
 
         bool findCycle = false;
         for(int u = 0; u < numCourses; u++)
-            if(vis[u] == NOT_PROCESSED) findCycle |= dfs(u, adjMat, vis);
+            if(vis[u] == NOT_PROCESSED) findCycle |= dfs(u, node_adjMat, vis);
 
         return !findCycle;
     }
