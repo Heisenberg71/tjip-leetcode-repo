5784fad03c6be2662a1ec529b3477fbdb19d0009
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-02-27 21:24:43 +0600

    Update Employee_Importance.cpp

--- week09/Employee_Importance.cpp
+++ week09/Employee_Importance.cpp
@@ -13,16 +13,16 @@ public:
 
 class Solution {
 private:
-    unordered_map <int, Employee*> id_employee;
-    int DFS(int u, vector<Employee*> employees){
+    int DFS(int u, vector<Employee*> employees, unordered_map <int, Employee*>& id_employee){
         int subtreeSum = id_employee[u] -> importance;
         for(auto v: id_employee[u] -> subordinates)
-            subtreeSum += DFS(v, employees);
+            subtreeSum += DFS(v, employees, id_employee);
         return subtreeSum;
     }
 public:
-    int getImportance(vector<Employee*> employees, int id) {
+    int getImportance(vector<Employee*> employees, int id){
+        unordered_map <int, Employee*> id_employee;
         for(auto emp: employees) id_employee[emp -> id] = emp;
-        return DFS(id, employees);
+        return DFS(id, employees, id_employee);
     }
 };
