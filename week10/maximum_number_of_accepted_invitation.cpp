ac4092abf29ab39a0818de9e7dff81cd66f1cbf6
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-03-21 18:58:21 +0600

    Create maximum_number_of_accepted_invitation.cpp

---
+++ week10/maximum_number_of_accepted_invitation.cpp
@@ -0,0 +1,20 @@
+// T.C: O(number of nodes)
+// M.C: O(recursion stack memorty)
+
+class Solution {
+private:
+    void verify(int& currIdx, vector<int>& preorder, int L, int R){
+        if(currIdx == preorder.size()) return;
+        int mid = preorder[currIdx];
+        if(mid < L || mid > R) return;
+        currIdx++;
+        verify(currIdx, preorder, L, mid - 1);
+        verify(currIdx, preorder, mid + 1, R);
+    }
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        int currIdx = 0;
+        verify(currIdx, preorder, INT_MIN, INT_MAX);
+        return (currIdx == preorder.size()) ? true: false;
+    }
+};
