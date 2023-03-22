4b7100c3c0f04e67823b2ad80cd39b961ea675db
Samin Yasir <69238722+Heisenberg71@users.noreply.github.com>
2023-03-22 10:35:53 +0600

    Update Coin_Change.cpp

--- week11/Coin_Change.cpp
+++ week11/Coin_Change.cpp
@@ -1,18 +1,20 @@
 // T.C: O(amount*total_coins)
 // M.C: O(amount)
 
+// coinCount[x] = y; Here, minimum y coins needed to make amount x
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector <int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
+        vector <int> coinCount(amount + 1, amount + 1);
+        coinCount[0] = 0;
         for(int i = 1; i <= amount; i++){
             for(int j = 0; j < n; j++){
                 if(i < coins[j]) continue;
-                dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                coinCount[i] = min(coinCount[i], 1 + coinCount[i - coins[j]]);
             }
         }
-        return (dp[amount] == amount + 1) ? -1 : dp[amount]; 
+        return (coinCount[amount] == amount + 1) ? -1 : coinCount[amount]; 
     }
 };
